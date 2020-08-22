24354bf4ba93289c53df94b4247512cd91134611
John Stassen <john.stassen@gmail.com>
2020-08-22 20:22:54 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,20 +4,3 @@ def first_challenge
     :capulet => {}
   }
 end
-
-def second_challenge
-   epic_tragedy = {
-     :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
-     },
-     :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
-     }
-   }
-end
